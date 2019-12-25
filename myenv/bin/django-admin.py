6648c1604c84c3c25beb9f09a7af2a9cc791457d
yamato100 <yamato100@gmail.com>
2019-12-26 00:18:54 +0900

    modify gitignore

--- myenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/mnt/c/Users/sunaf/Documents/4.programming/app/tennis_link/myenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
